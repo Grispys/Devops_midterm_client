11a2e4fc751fbec21e275e6bc1f003a5c8850dfb
Grispys <grispanimation@gmail.com>
2025-02-24 15:41:30 -0330

    created the domains files and started httprespcliapp

--- src/main/java/org/example/http/cli/HTTPRestCLIApp.java
+++ src/main/java/org/example/http/cli/HTTPRestCLIApp.java
@@ -1,4 +1,31 @@
 package org.example.http.cli;
 
+import org.example.domain.Airport;
+
+import java.util.List;
+
 public class HTTPRestCLIApp {
+
+
+    public String generateAirportReport() {
+        List<Airport> airports = getRestClient().getAllAirports();
+
+        StringBuffer report = new StringBuffer();
+
+        for (Airport airport : airports) {
+            report.append(airport.getName());
+            report.append(" - ");
+            report.append(airport.getCode());
+
+            if (airports.indexOf(airport) != (airports.size() - 1)) {
+                report.append(",");
+            }
+        }
+
+        System.out.println(report.toString());
+
+        return report.toString();
+    }
+
+
 }
